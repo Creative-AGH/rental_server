a0fbfee2a85c626e1bdf1dd643a54321d36f5f21
Adam Wasylewicz <adwas33@wp.pl>
2022-05-23 20:48:30 +0200

    feat: Added swagger implementation

--- src/main/java/pl/creative/rental_server/RentalServerApplication.java
+++ src/main/java/pl/creative/rental_server/RentalServerApplication.java
@@ -2,8 +2,10 @@ package pl.creative.rental_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RentalServerApplication {
 
     public static void main(String[] args) {
