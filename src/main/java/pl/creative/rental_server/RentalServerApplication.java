1217bd60f13d791806e5392c0a95a6f17594bab1
Adam Wasylewicz <adwas33@wp.pl>
2022-05-23 15:28:03 +0200

    Initial Commit

---
+++ src/main/java/pl/creative/rental_server/RentalServerApplication.java
@@ -0,0 +1,13 @@
+package pl.creative.rental_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/creative/rental_server/RentalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.creative.rental_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
