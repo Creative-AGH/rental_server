767891271d7b1154ad2e43cecf92dde1ffd2781a
Patryk <patrykjanas27@gmail.com>
2022-08-08 14:14:20 +0200

    refactor a place validation

--- src/main/java/pl/creative/rental_server/entities/Place.java
+++ src/main/java/pl/creative/rental_server/entities/Place.java
@@ -4,10 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
@@ -19,12 +16,13 @@ import java.util.List;
 @Table(name = "place")
 public class Place {
     @Id
+    @NotNull
+    @NotEmpty
+    @Column(unique = true)
     private String id;
     @NotNull
     @NotEmpty
     private String name;
-    @NotNull
-    @NotEmpty
     private String description;
 
 //    @ToString.Exclude
--- src/main/java/pl/creative/rental_server/placeManagement/dto/InputPlaceDto.java
+++ src/main/java/pl/creative/rental_server/placeManagement/dto/InputPlaceDto.java
@@ -2,8 +2,19 @@ package pl.creative.rental_server.placeManagement.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class InputPlaceDto {
+    //first check if not null, then if not empty, finally check if trimmed string is not blank
+    @NotNull(message = "Name can not be null")
+    @NotEmpty(message = "Name can not be empty")
+    @NotBlank(message = "Name can not be null or empty") //check if trimmed string has more than 0 digits
+    @Size(max = 255, message = "Name of the place must be shorter than {max} signs")
     private final String name;
+    @Size(max = 255, message = "Description of the place must be shorter than {max} signs")
     private final String description;
 }
