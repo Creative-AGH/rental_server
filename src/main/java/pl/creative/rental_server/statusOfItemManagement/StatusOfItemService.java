90d562f8c73c753d2d9d4da7379a7006004bbb9d
Patryk <patrykjanas27@gmail.com>
2022-08-13 20:06:43 +0200

    refactor

--- src/main/java/pl/creative/rental_server/statusOfItemManagement/StatusOfItemService.java
+++ src/main/java/pl/creative/rental_server/statusOfItemManagement/StatusOfItemService.java
@@ -1,6 +1,7 @@
 package pl.creative.rental_server.statusOfItemManagement;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.creative.rental_server.entities.StatusOfItem;
 import pl.creative.rental_server.statusOfItemManagement.dto.GetStatusOfItemDto;
@@ -11,10 +12,12 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class StatusOfItemService {
     private final StatusOfItemMapper statusOfItemMapper;
 
     public List<GetStatusOfItemDto> getAllStatusOfItem() {
+        log.info("Getting all status of item");
         List<GetStatusOfItemDto> statusOfItemList = new ArrayList<>();
         StatusOfItem[] values = StatusOfItem.values();
         for (StatusOfItem value : values) {
