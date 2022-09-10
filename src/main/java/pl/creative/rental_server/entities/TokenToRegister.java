8ce95793683cd66651b8adc8dc5f8f5a15ba6afe
adwas3213 <47114765+adwas3213@users.noreply.github.com>
2022-09-10 20:24:34 +0200

    Register feature (#10)
    Register features : 
    * fix: fixed connection via SSL to AGH post
    
    * feat: added register with email send feature
    
    * feat: added verify email support
    
    Co-authored-by: Patryk <patrykjanas27@gmail.com>

--- src/main/java/pl/creative/rental_server/entities/TokenToRegister.java
+++ src/main/java/pl/creative/rental_server/entities/TokenToRegister.java
@@ -1,13 +1,22 @@
 package pl.creative.rental_server.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 @Entity
-public class TokenToVerifyEmail {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenToRegister {
     @Id
-    private String id;
+    private String token;
+
     @OneToOne
     private Account account;
+
 }
