5fd6da867956724d23673de70a645b95ffb01c26
Patryk Janas <97175475+PatrykJanas27@users.noreply.github.com>
2022-09-22 19:58:55 +0200

    Add several unit tests (#20)
    * refactor
    
    * add application properties in need to do tests
    
    * completed CategoryServiceTest
    
    * add AccountRepositoryTest
    
    * refactor

--- src/main/java/pl/creative/rental_server/core/features/moderator/category/dto/FillCategoryDto.java
+++ src/main/java/pl/creative/rental_server/core/features/moderator/category/dto/FillCategoryDto.java
@@ -1,5 +1,6 @@
 package pl.creative.rental_server.core.features.moderator.category.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -8,6 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
+@AllArgsConstructor
 public class FillCategoryDto {
     @NotNull(message = "Category name can not be null")
     @NotEmpty(message = "Category name can not be empty")
--- src/main/java/pl/creative/rental_server/core/features/moderator/category/dto/GetCategoryDto.java
+++ src/main/java/pl/creative/rental_server/core/features/moderator/category/dto/GetCategoryDto.java
@@ -1,8 +1,10 @@
 package pl.creative.rental_server.core.features.moderator.category.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class GetCategoryDto {
     private String id;
     private String categoryName;
